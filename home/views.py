011220d2614c38a0a8b4863941f2577a8c668396
Alidarvishvand <133001519+Alidarvishvand@users.noreply.github.com>
2024-01-20 17:56:47 +0330

    models
     create queryset in django
    first ==> classes in models
    secode ==> register in admin.py
    Third ==> Variable construction

--- home/views.py
+++ home/views.py
@@ -1,8 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse 
+from.models import Todo
 # Create your views here.
+
+
+
+
 def sayhello(request):
     person = {'name': 'ali'}
     return render(request, 'hello.html',context=person)
 def home(request):
-    return render(request, 'home.html')
+    all = Todo.objects.all()
+    return render(request, 'home.html',{'all':all})
